3dd874a08db95342efffa9f47a1ffd88ece1aae8
Vicuna <45003538+764069075@users.noreply.github.com>
2025-03-03 21:56:08 +0800

    Update sessionUtils.py

--- utils/sessionUtils.py
+++ utils/sessionUtils.py
@@ -1,8 +1,7 @@
-from aiohttp import ClientSession, TCPConnector, DummyCookieJar, ClientTimeout
+from aiohttp import ClientSession, TCPConnector, DummyCookieJar
 from utils.configUtils import config
 
 
 conn = TCPConnector(limit_per_host= 0, limit= 0, use_dns_cache= True, ttl_dns_cache= None, enable_cleanup_closed= True, force_close= False)
-timeout = ClientTimeout(6 , conn)
 jar = DummyCookieJar()
 session = ClientSession(proxy=config["PROXY"], cookie_jar= jar, trust_env= True)
