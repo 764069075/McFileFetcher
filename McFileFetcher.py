e5c0f268de4706bf35fcec684031b05364705ba3
Vicuna <45003538+764069075@users.noreply.github.com>
2025-06-05 18:41:09 +0800

    更新 McFileFetcher.py
    增加更多提示语

--- McFileFetcher.py
+++ McFileFetcher.py
@@ -2,13 +2,19 @@ from os import system
 
 
 def welcome():
-    print('''\033[36m
+    print('\033[36m')
+    print('''
  欢迎使用 McFileFetcher MC文件一键下载工具
 
             github 开源地址：
 
 https://github.com/764069075/McFileFetcher
-\033[0m''')
+
+            官方使用手册Wiki地址：
+
+https://github.com/764069075/McFileFetcher/wiki/使用教程
+''')
+    print('\033[0m')
 welcome()
 system('pause')
 
@@ -31,7 +37,7 @@ async def main():
     failure = length-success
     print(f'下载结束，耗时：{waste:.2f}s 成功：{success} 失败：{failure}\033[0m')
     if failure == length:
-        print('\033[33m【提示】全部失败了? 可以检查配置文件中代理是否正确配置\033[0m')
+        print('\033[33m【提示】全部失败了? 可以检查配置文件中代理是否正确配置，或者前往官方文档查看解决方案（https://github.com/764069075/McFileFetcher/wiki/使用教程）\033[0m')
     resultcsv(config['DOWN_STATE_FILE_NAME'],results,waste,success,length,fileinfos)
     system('pause')
 
